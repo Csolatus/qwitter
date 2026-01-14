ff322d154a4fb8c446c87dc7baa248e2288c71ce
Csolatus <lucas.lanoue@efrei.net>
2026-01-14 15:34:23 +0100

    implementation de l'onglet facturation, mise en place de l'environnement docker pour standardiser le deploiement du projet, mise a jour de l'entité user pour implementer user interface, et ajout des methodes requises

---
+++ src/Form/BillingType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BillingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        // Formulaire vide pour l'instant (mock de facturation)
+        /*
+        $builder
+            ->add('field_name')
+        ;
+        */
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
