a2a279e369360f13c558c5cb018c3786c5f25782
Matteixou <matthieu.feracho@efrei.net>
2026-01-15 19:04:52 +0100

    Modification concernant mes données reference + versions migration

--- migrations/Version20260115175611.php
+++ migrations/Version20260115175611.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260115104737 extends AbstractMigration
+final class Version20260115175611 extends AbstractMigration
 {
     public function getDescription(): string
     {
